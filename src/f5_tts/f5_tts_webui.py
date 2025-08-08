47080d631981c392992bd4d40c1aed5ec9e060e6
VYNCX <117465410+VYNCX@users.noreply.github.com>
2025-08-08 17:49:59 +0700

    Update f5_tts_webui.py

--- src/f5_tts/f5_tts_webui.py
+++ src/f5_tts/f5_tts_webui.py
@@ -10,17 +10,14 @@ import argparse
 import os
 
 from f5_tts.infer.utils_infer import (
-    hop_length,
     infer_process,
     load_model,
     load_vocoder,
     preprocess_ref_audio_text,
     remove_silence_for_generated_wav,
     save_spectrogram,
-    transcribe,
-    target_sample_rate,
 )
-from f5_tts.model import DiT, UNetT
+from f5_tts.model import DiT
 from f5_tts.model.utils import seed_everything
 import torch
 from f5_tts.cleantext.number_tha import replace_numbers_with_thai
@@ -30,17 +27,22 @@ from f5_tts.infer.infer_gradio import *
 
 #ถ้าอยากใช้โมเดลที่อัพเดทใหม หรือโมเดลภาษาอื่น สามารถแก้ไขโค้ด Model และ Vocab เช่น default_model_base = "hf://VIZINTZOR/F5-TTS-THAI/model_350000.pt"
 default_model_base = "hf://VIZINTZOR/F5-TTS-THAI/model_1000000.pt"
-fp16_model_base = "hf://VIZINTZOR/F5-TTS-THAI/model_650000_FP16.pt"
+v2_model_base = "hf://VIZINTZOR/F5-TTS-TH-v2/model_250000.pt"
 vocab_base = "./vocab/vocab.txt"
+vocab_ipa_base = "./vocab/vocab_ipa.txt"
 
-model_choices = ["Default", "FP16", "Custom"]
+model_choices = ["Default", "V2", "Custom"]
 
 global f5tts_model
 f5tts_model = None
 
-def load_f5tts(ckpt_path, vocab_path=vocab_base):
-    F5TTS_model_cfg = dict(dim=1024, depth=22, heads=16, ff_mult=2, text_dim=512, conv_layers=4)
-    model = load_model(DiT, F5TTS_model_cfg, ckpt_path, vocab_file = vocab_path if os.path.exists(vocab_base) else str(cached_path("hf://VIZINTZOR/F5-TTS-THAI/vocab.txt")), use_ema=True)
+def load_f5tts(ckpt_path, vocab_path=vocab_base, model_type="v1"):
+    if model_type == "v1":
+        F5TTS_model_cfg = dict(dim=1024, depth=22, heads=16, ff_mult=2, text_dim=512, text_mask_padding=False, conv_layers=4, pe_attn_head=1)
+    elif model_type == "v2":
+        F5TTS_model_cfg = dict(dim=1024, depth=22, heads=16, ff_mult=2, text_dim=512, text_mask_padding=True, conv_layers=4, pe_attn_head=None)
+        vocab_path = "./vocab/vocab_ipa.txt"
+    model = load_model(DiT, F5TTS_model_cfg, ckpt_path, vocab_file = vocab_path, use_ema=True)
     print(f"Loaded model from {ckpt_path}")
     return model
 
@@ -54,13 +56,16 @@ def update_custom_model(selected_model):
 def load_custom_model(model_choice,model_custom_path):
     torch.cuda.empty_cache()
     global f5tts_model
-    model_path = default_model_base if model_choice == "Default" else fp16_model_base
-    #f5tts_model = load_f5tts(str(cached_path(model_path)))
+    model_path = default_model_base if model_choice == "Default" else v2_model_base
     if model_choice == "Custom":
         f5tts_model = load_f5tts(str(cached_path(model_custom_path)))
         return f"Loaded Custom Model {model_custom_path}"
     else:
-        f5tts_model = load_f5tts(str(cached_path(model_path)))
+        f5tts_model = load_f5tts(
+            str(cached_path(model_path)),
+            vocab_path = vocab_ipa_base if model_choice == "V2" else vocab_base,
+            model_type = "v2" if model_choice == "V2" else "v1"
+        )
         return f"Loaded Model {model_choice}"
     
 def infer_tts(
@@ -74,7 +79,7 @@ def infer_tts(
     cfg_strength=2,
     max_chars=250,
     seed=-1,
-    no_ref_audio=False
+    lang="TH"
 ):
     global f5tts_model
     if f5tts_model is None:
@@ -94,25 +99,22 @@ def infer_tts(
         return gr.update(), gr.update(), ref_text, output_seed
     
     ref_audio, ref_text = preprocess_ref_audio_text(ref_audio_orig, ref_text)
-    cross_fade_duration = float(cross_fade_duration)
     
-    gen_text_cleaned = process_thai_repeat(replace_numbers_with_thai(gen_text))
-    
+    gen_text_cleaned = process_thai_repeat(replace_numbers_with_thai(gen_text)) if lang == "TH" else gen_text
+
     final_wave, final_sample_rate, combined_spectrogram = infer_process(
         ref_audio,
         ref_text,
         gen_text_cleaned,
         f5tts_model,
         vocoder,
-        cross_fade_duration=cross_fade_duration,
+        cross_fade_duration=float(cross_fade_duration),
         nfe_step=nfe_step,
         speed=speed,
         progress=gr.Progress(),
         cfg_strength=cfg_strength,
-        target_rms=0.1,
-        sway_sampling_coef=-1,
         set_max_chars=max_chars,
-        no_ref_audio=no_ref_audio
+        use_ipa=True if lang == "TH-IPA" else False
     )
 
     if remove_silence:
@@ -139,7 +141,7 @@ def transcribe_text(input_audio="",translate=False,model="large-v3-turbo",comput
     return output_text
 
 def create_gradio_interface():
-    with gr.Blocks(title="F5-TTS ไทย",theme=gr.themes.Ocean()) as demo:
+    with gr.Blocks(title="F5-TTS ภาษาไทย",theme=gr.themes.Ocean()) as demo:
         gr.Markdown("# F5-TTS ภาษาไทย")
         gr.Markdown("สร้างคำพูดจากข้อความ ด้วย Zero-shot TTS หรือ เสียงต้นฉบับ ภาษาไทย.")
 
@@ -163,6 +165,7 @@ def create_gradio_interface():
                     generate_btn = gr.Button("สร้าง",variant="primary")
 
                     with gr.Accordion(label="ตั้งค่า"):
+                        lang_input = gr.Radio(label="Language",choices=["TH","TH-IPA","EN"],value="TH",info="การประมวลผลข้อความภาษา")
                         remove_silence = gr.Checkbox(label="Remove Silence", value=True)
                         speed = gr.Slider(label="ความเร็ว", value=1, minimum=0.3, maximum=1.5, step=0.1)
                         cross_fade_duration = gr.Slider(label="Cross Fade Duration", value="0.15", minimum=0, maximum=1, step=0.05)
@@ -171,7 +174,6 @@ def create_gradio_interface():
                         max_chars = gr.Number(label="ตัวอักษรสูงสุดต่อส่วน", minimum=50, maximum=1000, value=250,
                                             info="จำนวนตัวอักษรสูงสุดที่ใช้ในการแบ่งส่วน สำหรับข้อความยาวๆ")
                         seed = gr.Number(label="Seed", value=-1, precision=0, info="-1 = สุ่ม Seed")
-                        no_ref_audio = gr.Checkbox(label="เสียงดั้งเดิม", value=False,info="ใช้เสียงที่ไม่ผ่านการโคลนเสียงจากโมเดล")
                         
                 with gr.Column():
                     output_audio = gr.Audio(label="เสียงที่สร้าง", type="filepath")
@@ -247,7 +249,7 @@ def create_gradio_interface():
                     cfg_strength,
                     max_chars,
                     seed,
-                    no_ref_audio
+                    lang_input
                 ],
                 outputs=[
                     output_audio,
